5887b260316d95cb50d9884cca4b6167ced8e989
Patrick Balsiger <frupii@gmail.com>
2023-10-31 09:34:52 +0100

    Add demo mode

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ DB_FILE=/var/db/dev.db
 if [ ! -f "$DB_FILE" ]; then
 	echo "$DB_FILE does not exist - initialize database"
 	npx prisma migrate dev --name nitro
+	if [[ $NP_DEMO -eq 1 ]]; then
+	echo "Apply demo seed"
+		npx nitropage seed-demo
+	fi
 else
 	echo "$DB_FILE exist - update database"
 	npx prisma migrate deploy
